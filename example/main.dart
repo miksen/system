0b875e3eaa33b8d6a9be09929b37549ea60f2b0d
Michael Thomsen <m@svth.dk>
2021-01-29 13:20:26 +0100

    Fix a lint

--- example/main.dart
+++ example/main.dart
@@ -2,8 +2,8 @@ import 'package:system/system.dart';
 
 // Use the system `open` command to lauch the system default web browser on the
 // Url dart.dev.
-main() async {
-  bool result = System.invoke('open http://dart.dev');
+void main() async {
+  var result = System.invoke('open http://dart.dev');
   if (result) {
     print('Succesfully launched dart.dev');
   } else {
